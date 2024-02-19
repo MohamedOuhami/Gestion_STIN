f854831153bc91fc0dd91aeb4935b2d87ca4ce37
MohamedOuhami <mohamed.ouhami2001@gmail.com>
2024-02-19 21:26:47 +0100

    first commit

---
+++ backend_gestion_stin/src/main/java/com/ensaj/backend_gestion_stin/BackendGestionStinApplication.java
@@ -0,0 +1,13 @@
+package com.ensaj.backend_gestion_stin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGestionStinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGestionStinApplication.class, args);
+	}
+
+}
---
+++ backend_gestion_stin/src/test/java/com/ensaj/backend_gestion_stin/BackendGestionStinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensaj.backend_gestion_stin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGestionStinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
